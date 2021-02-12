5a33c3e27f9da8c9c1e3a493172eb5c6659db3dd
threevanny <ggeovannyreyes@gmail.com>
2021-02-11 19:04:52 -0500

    Create service auth, login and signup page

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,9 +1,76 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const API = 'https://localhost:3000/api/v1';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+
+  //Questions request 
+  getQuestionById(id: String) {
+    return this.http.get(`${API}/question/${id}`);
+  }
+
+  getQuestions() {
+    return this.http.get(`${API}/questions/`);
+  }
+
+  newQuestion(data: any) {
+    return this.http.post(`${API}/question/new/`, data);
+  }
+
+  updateQuestion(id: String, data: any) {
+    return this.http.put(`${API}/question/update/${id}`, data);
+  }
+
+  deleteQuestion(id: String) {
+    return this.http.delete(`${API}/question/delete/${id}`);
+  }
+
+  //Replies request
+  getReplyById(id: String) {
+    return this.http.get(`${API}/reply/${id}`);
+  }
+
+  getReplies() {
+    return this.http.get(`${API}/replies/`);
+  }
+
+  newReply(data: any) {
+    return this.http.post(`${API}/reply/new/`, data);
+  }
+
+  updateReply(id: String, data: any) {
+    return this.http.put(`${API}/reply/update/${id}`, data);
+  }
+
+  deleteReply(id: String) {
+    return this.http.delete(`${API}/reply/delete/${id}`);
+  }
+
+  //Avatar request
+  getAvatarById(id: String) {
+    return this.http.get(`${API}/avatar/${id}`);
+  }
+
+  getAvatars() {
+    return this.http.get(`${API}/avatars/`);
+  }
+
+  newAvatar(data: any) {
+    return this.http.post(`${API}/avatar/new/`, data);
+  }
+
+  updateAvatar(id: String, data: any) {
+    return this.http.put(`${API}/avatar/update/${id}`, data);
+  }
+
+  deleteAvatar(id: String) {
+    return this.http.delete(`${API}/avatar/delete/${id}`);
+  }
+
 }
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+const API = 'https://localhost:3000/api/v1';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+
+  login(data: any) {
+    return this.http.post(`${API}/user/login`, data);
+  }
+
+  logout(data: any) {
+    return this.http.post(`${API}/user/logout`, data);
+  }
+
+  signup(data: any) {
+    return this.http.get(`${API}/user/signup`, data);
+  }
+
 }
